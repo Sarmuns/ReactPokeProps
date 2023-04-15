b968129745038e591f1f3c84a66158fb7f39e97c
Samuel Alcântara Fontenele Rocha <samuel.rocha@dellead.com>
2023-04-15 16:15:24 -0300

    styling touches

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -16,9 +16,11 @@ const App = () => {
   }
 
   return (
-    <div>
-      <button onClick={resetClick} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Reset</button>
-      <button onClick={changeState} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Switch</button>
+    <div className='bg-gray-900'>
+      <div className='flex spacex-2 py-2 px-2 justify-between w-1/6'>
+        <button onClick={resetClick} className='bg-blue-500 hover:bg-blue-700 hover:border-2  border-neutral-950  text-white font-bold py-2 px-4 rounded '>Reset</button>
+        <button onClick={changeState} className='bg-blue-500 hover:bg-blue-700 hover:border-2  border-neutral-950 text-white font-bold py-2 px-4 rounded '>Switch</button>
+      </div>
       <div className='mt-4 flex flex-wrap gap-3 mx-auto justify-center'>
 
         {pokemons.map((pokemon, index) =>
